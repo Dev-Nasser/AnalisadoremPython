0400544d3de8bae417a1065c85ab8794677ce327
Dev Nasser <93291368+Dev-Nasser@users.noreply.github.com>
2021-11-02 16:07:46 -0300

    atualização

--- funções teste .py
+++ funções teste .py
@@ -1,6 +1,6 @@
 def l (x):
     tamanho = len(x)
-    print("O teu nome tem o tamanho de {} letras".format(tamanho))
+    print("A sua string tem o tamanho de {} letras".format(tamanho))
     return
 
 def espaço (e):
@@ -9,17 +9,15 @@ def espaço (e):
     return
 
 def analisadorcompleto (x , y):
-    print ("É {} que a sua variavel é uma string".format(x.isalpha()))
-    print ("É {} que a sua varivel está em minusculas".format(x.islower()))
-    print ("Esta em maiusculas? {} ".format (x.isupper()))
+    print ("É {} que a sua variavel está em minusculas".format(x.islower()))
+    print ("É {} que está em maiusculas ".format (x.isupper()))
     print ("A letra que você digitou está na posição {}".format(x.find(y)))
+    print ("Esta letra aparece {} vezes".format(x.count(y)))
+    print ("Você digitou a letra {}".format(y))
+    print ("Sua string em maiusculas é {}".format(x.upper()))
     return
 
 
-nome = str(input("Digite a string que vc deseja que seja analisada.")).upper()
-print ("Digite a letra que deseja saber onde está na palavra")
-letra = str(input()).upper()
-print (input(" \nO caractere {} se encontra nessa string por {} vezes \n Tecle algo para continuar essa analise".format(letra, nome.count(letra))))
-l(nome)
-espaço(nome)
+nome = str(input("Digite a string que vc deseja que seja analisada."))
+letra = str(input("Digite a letra que você quer que seja analisada"))
 analisadorcompleto(nome, letra)
